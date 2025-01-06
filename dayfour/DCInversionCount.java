5b29dd8392258a5615410dea4ca21b02b65a1884
razzaksr <razzaksr@gmail.com>
2025-01-06 16:27:52 +0530

    dc

---
+++ dayfour/DCInversionCount.java
@@ -0,0 +1,71 @@
+package dayfour;
+
+import java.util.Arrays;
+
+public class DCInversionCount {
+    // O(nlogn)
+    public static int mergeAndCount(int[] original, int[] temporary, int start, int middle, int end){
+        int inversions = 0;// current recursion count
+        //System.out.println(Arrays.toString(Arrays.copyOfRange(original, start, end+1)));
+        //System.out.println("Start "+start+" middle "+middle+" end "+end);
+        int westIndex = start, eastIndex = middle + 1, current = start;
+        System.out.println("West Index "+westIndex+" East Index "+eastIndex+" current pos "+current);
+        while(westIndex<=middle&&eastIndex<=end){
+            if(original[westIndex]<=original[eastIndex]){
+                temporary[current++] = original[westIndex++];
+            }
+            else{
+                System.out.println(original[westIndex]+" "+original[eastIndex]+" pair");
+                temporary[current++] = original[eastIndex++];
+                inversions += (middle-westIndex+1);
+                System.out.println((middle-westIndex+1)+" done");
+            }
+        }
+        // Copy remaining elements of left subarray, if any
+        while (westIndex <= middle) {
+            temporary[current++] = original[westIndex++];
+        }
+
+        // Copy remaining elements of right subarray, if any
+        while (eastIndex <= end) {
+            temporary[current++] = original[eastIndex++];
+        }
+
+        // Copy sorted subarray back to original array
+        for (westIndex = start; westIndex <= end; westIndex++) {
+            original[westIndex] = temporary[westIndex];
+        }
+        System.out.println("Current Inversion Count "+inversions);
+        return inversions;
+    }
+
+    private static int divide(int[] arr, int[] temporary, int left, int right) {
+        int inversions = 0;// total count
+        System.out.println(Arrays.toString(Arrays.copyOfRange(arr, left, right+1)));
+        if (left < right) {
+            // int mid = (left + right) / 2;
+            int mid = left+(right-left) / 2;
+
+            // Count inversions in left subarray
+            inversions += divide(arr, temporary, left, mid);
+
+            // Count inversions in right subarray
+            inversions += divide(arr, temporary, mid + 1, right);
+
+            // Count split inversions
+            inversions += mergeAndCount(arr, temporary, left, mid, right);
+        }
+        return inversions;
+    }
+
+
+    public static void main(String[] args) {
+        // int[] arr = {45,91,10,67,21,6,11};
+        // int[] arr = {1, 9, 6, 4, 5};
+        int[] arr = {9,4,2,13,10,6,12};
+        int[] temporary = new int[arr.length];
+        int count = divide(arr, temporary, 0, arr.length-1);
+        System.out.println(count);
+        //System.out.println(inversionCount(arr));
+    }
+}
